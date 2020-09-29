fea61d172506b6e6887233cb478a9cad2c12eef2
Junique Virgile <vjunique@starclay.fr>
2020-09-29 23:27:53 +0200

    first try

--- src/App.js
+++ src/App.js
@@ -2,35 +2,42 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { ApolloServer, gql } from 'apollo-server-express';
+import typeDefs from './graphql/schema';
+import resolvers from './graphql/resolvers';
 // Tools
 import logger from '@tools/logger';
 // Middleware
 import { loggerMiddleware } from '@middleware/logger.middleware.js';
-// Controller
-import { AuthController } from '@controllers/auth';
-import { DashController } from '@controllers/dashboard';
+// Db
+import db from './db';
 
 dotenv.config();
 
 class App {
     constructor() {
         this.app = express();
-        this.controllers = [
-            new AuthController(),
-            new DashController(),
-        ];
+        this.server = new ApolloServer({
+            typeDefs: gql(typeDefs),
+            resolvers,
+            context: { db },
+        });
+
         this.initializeMiddlewares = this.initializeMiddlewares.bind(this);
-        this.initializeControllers = this.initializeControllers.bind(this);
         this.initializeMiddlewares();
-        this.initializeControllers();
     }
 
     listen() {
-        this.app.listen(process.env.PORT, () => {
-            logger.info('/*************** *************** ***************/');
-            logger.info('/*************** STARTING SERVER ***************/');
-            logger.info('/*************** *************** ***************/');
-            logger.info(`App listening HTTP on the port ${process.env.PORT}`);
+        db.sequelize.sync().then(() => {
+            this.app.listen(process.env.PORT, () => {
+                logger.info('/*************** *************** ***************/');
+                logger.info('/*************** STARTING SERVER ***************/');
+                logger.info('/*************** *************** ***************/');
+                logger.info(`🚀 Server ready and listening HTTP on the port ${process.env.PORT}`);
+            }).on('error', (err) => {
+                logger.error(err);
+                process.exit(1);
+            });
         }).on('error', (err) => {
             logger.error(err);
             process.exit(1);
@@ -42,20 +49,10 @@ class App {
         this.app.use(bodyParser.json({limit: '50mb', extended: true}));
         this.app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
         this.app.use(loggerMiddleware);
-    }
 
-    initializeControllers() {
-        this.app.get('/', (req, res) => {
-            res.send('Hello in boilerplate-graphql api !');
-        });
-        this.controllers.forEach((controller) => {
-            this.app.use('/', controller.router);
-        });
-        this.app.get('*', (req, res) => {
-            res.send('404 Not Found');
-        });
-    }
+        this.server.applyMiddleware( this.app );
 
+    }
 }
 
 export { App };
