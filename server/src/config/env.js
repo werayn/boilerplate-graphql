f25dcf61b770ebc23391e705631c905d0b485f8a
Junique Virgile <vjunique@starclay.fr>
2020-10-16 12:47:35 +0200

    move to server folder

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -4,11 +4,11 @@ dotenv.config();
 
 export const env = {
     PORT: process.env.PORT || '3011',
-    DB_HOST: process.env.DB_HOST || '192.168.xxx.xxx',
+    DB_HOST: process.env.DB_HOST || '192.168.222.204',
     DB_PORT: process.env.DB_PORT || '5432',
-    DB_NAME: process.env.DB_NAME || 'xx',
+    DB_NAME: process.env.DB_NAME || 'dge',
     DB_USER: process.env.DB_USER || 'ghpsj',
-    DB_PASSWORD: process.env.DB_PASSWORD || 'xxx',
+    DB_PASSWORD: process.env.DB_PASSWORD || 'starclay2018',
     DB_DIALECT: process.env.DB_DIALECT || 'postgres',
 
     JWT_ENCRYPTION: process.env.JWT_ENCRYPTION || 'secureKey',
